298d22c73f623e6b14afdeb8a94b0a4595e1c939
Evan Chisholm <elchisholm248@gmail.com>
2023-09-16 00:41:24 -0400

    added server and cohere api call

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+import express from "express";
+import { analyzeBiasFromURL } from "./lib/cohere.js";
+const app = express();
+
+app.get("/", async (req, res) => {
+    const publisherBias = await analyzeBiasFromURL(req.query.url);
+    res.json({
+        publisherBias,
+    });
+});
+
+app.listen(8080, () => console.log("running on port 8080"));
