bc9269aa27d74bbf20b1c1bced747cf32a7fb914
BuildTools <unconfigured@null.spigotmc.org>
2023-09-16 00:45:57 -0400

    it works now

--- scraper.js
+++ scraper.js
@@ -1,17 +1,38 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-var hardCodeSiteUrl = "https://www.nytimes.com/puzzles/spelling-bee";
+let hardCodeSiteUrl = "https://www.cnn.com/2023/09/15/americas/el-chapo-son-extradited-mexico-us-hnk-intl/index.html";
 
 const fetchData = async (siteUrl) => {
-  const result = await axios.get(siteUrl);
-  //console.log(result.data);
-  return cheerio.load(result.data);
+  try {
+    const result = await axios.get(siteUrl);
+    return result.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; 
+  }
+};
+
+const extractArticleContent = (html) => {
+  const $ = cheerio.load(html);
+  const articleContent = [];
+
+  $("p").each((index, element) => {
+    articleContent.push($(element).text());
+  });
+  console.log("testes 2");
+  console.log(articleContent.join("\n"));
+  return articleContent.join("\n");
 };
 
 const getResults = async () => {
-  var $ = await fetchData(hardCodeSiteUrl);
-
+  try {
+    const html = await fetchData(hardCodeSiteUrl);
+    const articleText = extractArticleContent(html);
+    console.log(html, articleText);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 };
-
+getResults();
 module.exports.getResults = getResults;
